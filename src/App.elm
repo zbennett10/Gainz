a6882e1d4f997e97c17c1c626cb3f851457096d4
Zachary Bennett <zbennett10@hotmail.com>
2017-06-11 22:06:24 -0700

    added update methods, started backend server connectivity

--- src/App.elm
+++ src/App.elm
@@ -12,9 +12,15 @@ import Types exposing (Exercise)
 type alias Model =
     { exercises : List Exercise
     , currentExercise : Exercise
+    , stagedMuscleGroups : List String
     }
 
-
+initialModel : Model
+initialModel =
+    { exercises = [ Exercise "Bicep Curl" 12 4 "2017-06-11T:00:00:00Z" ["Biceps"] ]
+    , currentExercise = Exercise "Skullcrushers" 12 4 "2017-06-11T:00:00:00Z" ["Triceps"] 
+    , stagedMuscleGroups = [" "]
+    }
 
 init : ( Model, Cmd Msg )
 init =
@@ -26,7 +32,8 @@ init =
 
 
 type Msg
-    = PostExercise Exercise
+    = PostExercise
+    | StageMuscleGroup String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -34,6 +41,9 @@ update message model =
     case message of
         PostExercise currentExercise ->
             ( model, Cmd.none )
+        
+        StageMuscleGroup selection ->
+            ( { model | stagedMuscleGroups = (addMuscleGroupToModel model.stagedMuscleGroups selection) }, Cmd.none )
 
 
 
@@ -42,29 +52,57 @@ update message model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ]
+    div [ class "container text-center" ]
         [ h1 [] [ text "Gainz" ]
         , p [] [ text <| toString model ]
-        , button
-            [ class "btn btn-primary"
-            , onClick PostExercise model.currentExercise3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        , div [] 
+            [ div [ class "form-group form-inline" ]
+                [ label [] [ text "Name" ]
+                , input [] []
+                ]
+            , div [ class "form-group form-inline" ]
+                [ label [] [ text "Sets" ] 
+                , input [] []
+                ]
+            , div [ class "form-group form-inline" ]
+                [ label [] [ text "Reps" ]
+                , input [] []
+                ]
+            , div [ class "form-group form-inline" ]
+                [ label [] [ text "Date" ] 
+                , input [ type_ "date" ] []
+                ]
+            , div [ class "form-group form-inline" ]
+                [ label [] [ text "Muscle Groups" ]
+                , select [ onInput StageMuscleGroup ]
+                    [ option [] [ text "Biceps" ]
+                    , option [] [ text "Triceps" ]
+                    , option [] [ text "Forearms" ]
+                    , option [] [ text "Shoulders" ]
+                    , option [] [ text "Quadriceps" ]
+                    , option [] [ text "Hamstrings" ]
+                    , option [] [ text "Abs" ]
+                    , option [] [ text "Chest" ]
+                    , option [] [ text "Back" ]
+                    ]
+                , textarea [] [ text (showStagedMuscleGroups model) ]
+                ]
+            , button [ class "btn btn-lg btn-success"
+                     , onClick PostExercise
+                     ] 
+                     [ text "Submit Exercise" ]
             ]
-            [ text "+ 1" ]
         ]
+
+
+
+-- helpers
+
+
+addMuscleGroupToModel : List String -> String -> List String
+addMuscleGroupToModel muscles newGroup =
+    newGroup :: muscles
+
+showStagedMuscleGroups : Model -> String
+showStagedMuscleGroups model =
+    String.join "," model.stagedMuscleGroups
